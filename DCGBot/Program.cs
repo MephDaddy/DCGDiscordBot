7871f946df648bf0e036053334fae6682390dfa2
MephDaddy <mephdaddy@live.com>
2018-08-12 21:49:14 -0500

    Updated program.cs
    Hello World to Hello Discord

--- DCGBot/Program.cs
+++ DCGBot/Program.cs
@@ -6,7 +6,7 @@ namespace DCGBot
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Discord!");
         }
     }
 }
