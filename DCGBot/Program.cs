c5221ede225612910146234b6e85e26bc72b056c
MephDaddy <mephdaddy@live.com>
2018-08-12 21:37:56 -0500

    Second Commit

---
+++ DCGBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DCGBot
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
