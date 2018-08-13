9df947c1f349b0dd6f324525ac741940e49e57ec
MephDaddy <mephdaddy@live.com>
2018-08-13 02:01:34 -0500

    Finished Part 3 of Tutorial
    Added IDataStorage, InMemoryStorage, and Unity Helper

--- DCGBot/Program.cs
+++ DCGBot/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 
+
 namespace DCGBot
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
+            Unity.RegisterTypes();
             Console.WriteLine("Hello Discord!");
         }
+
     }
 }
---
+++ DCGBot/Unity.cs
@@ -0,0 +1,36 @@
+﻿using DCGBot.Storage;
+using DCGBot.Storage.Implementations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Unity;
+using Unity.Resolution;
+
+namespace DCGBot
+{
+    public static class Unity
+    {
+        private static UnityContainer _container;
+
+        public static UnityContainer Container
+        {
+            get
+            {
+                if (_container == null)
+                    RegisterTypes();
+                return _container;
+            }
+        }
+
+        public static void RegisterTypes()
+        {
+            _container = new UnityContainer();
+            _container.RegisterType<IDataStorage, InMemoryStorage>();
+        }
+
+        public static T Resolve<T>()
+        {
+            return (T)Container.Resolve(typeof(T), string.Empty, new CompositeResolverOverride());
+        }
+    }
+}
